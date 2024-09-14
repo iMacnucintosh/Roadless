56c18a91559ee0e47da861797708994c93a867f9
iMacnucintosh <iMacnucintosh@users.noreply.github.com>
2024-09-14 14:55:04 +0000

    deploy: 6e1e8497ad51566da3d8a804ba4cadd4a55613d2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1929059564"
+    serviceWorkerVersion: "3853975093"
   }
 });
